3e354408165ff99e4bf90f5f074c00e6f33328c6
Josh <joshmh7128@gmail.com>
2022-07-15 22:56:47 -0400

    more updates

--- Assets/Scripts/CameraScreenshake.cs
+++ Assets/Scripts/CameraScreenshake.cs
@@ -18,7 +18,14 @@ public class CameraScreenshake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // process the shake
         ProcessShake();
+
+        // test
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ScreenShake(1,1,0.1f, 0.25f, 6f);
+        }
     }
 
     // trigger a new screenshake
@@ -33,16 +40,36 @@ public class CameraScreenshake : MonoBehaviour
         _interval = interval;
         // length = how long the shake lasts
         _length = length;
+        // run the functions
+        StartCoroutine(MoveTarget());
+    }
 
-        // run the function
-
+    IEnumerator MoveTarget()
+    {
+        if (_length >= 0)
+        {
+            yield return new WaitForSeconds(_interval);
+            float xr = Random.Range(-_intensity, _intensity);
+            float yr = Random.Range(-_intensity, _intensity);
+            float zr = Random.Range(-_intensity, _intensity);
+            targetPos = new Vector3(home.x + xr, home.y + yr, 0);
+            // calculate length
+            _length -= _interval;
+            // loop
+            StartCoroutine(MoveTarget());
+        } else
+        {
+            targetPos = home;
+        }
     }
 
     // move the camera around
     void ProcessShake()
     {
         // lerp at our intensity
-        Vector3.Lerp(transform.position, targetPos, currentVelocity * Time.deltaTime);
+        Vector3.Lerp(transform.position, targetPos, _velocity * Time.deltaTime);
         // lower our velocity by our delta
+        _velocity -= _velocityDelta;
+
     }
 }
